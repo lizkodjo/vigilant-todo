7e0b05c61330a61d2ec5177af8eb69110e02adcf
LizKodjo <ellizakodjo@outlook.com>
2025-11-10 20:29:53 +0000

    fix: Added directory to python path

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,6 +1,11 @@
+import os
+import sys
 from fastapi.testclient import TestClient
 import pytest
 from app.main import app
+
+# Add the app directory to Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 @pytest.fixture
