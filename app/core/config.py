04ba5d2c593e47dea305dab8070ccf0a97598223
LizKodjo <ellizakodjo@outlook.com>
2025-11-10 22:37:56 +0000

    fix: Fixed a typo in config

--- app/core/config.py
+++ app/core/config.py
@@ -16,7 +16,7 @@ class Settings(BaseSettings):
     # Security
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM: str = os.getenv("ALGORITHM")
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
     # CORS
     CORS_ORIGINS: list = ["http://localhost:3000"]
